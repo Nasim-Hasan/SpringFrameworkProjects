8559192a93ff6789cfef8ad187cb68f43aa10046
Nasim-Hasan-BJIT <nasim.hasan@bjitgroup.com>
2024-02-25 22:35:52 +0600

    Tutorial-Server Source Code Added and Also Modified Some Source Codes

---
+++ Tutorial-Server/src/main/java/com/example/TutorialServer/Model/Tutorial.java
@@ -0,0 +1,30 @@
+package com.example.TutorialServer.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+
+    public Tutorial(String title, String description, boolean published) {
+           this.title = title;
+           this.description=description;
+           this.published=published;
+    }
+
+}
---
+++ Tutorial-Server/src/test/java/com/example/TutorialServer/TutorialServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TutorialServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
