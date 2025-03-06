d53ee6a97dead7fc53267afd4fff565b370060fa
Nasim-Hasan <nasimcse2k5@gmail.com>
2025-03-06 06:04:40 +0600

    Adding Some Spring Boot Source Codes for R&D

---
+++ spring5-webflux-rest-create-vendor/src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
@@ -0,0 +1,39 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+
+import guru.springframework.spring5webfluxrest.domain.Vendor;
+import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by jt on 12/30/17.
+ */
+@RestController
+public class VendorController {
+
+    private final VendorRepository vendorRepository;
+
+    public VendorController(VendorRepository vendorRepository) {
+        this.vendorRepository = vendorRepository;
+    }
+
+    @GetMapping("/api/v1/vendors")
+    Flux<Vendor> list(){
+        return vendorRepository.findAll();
+    }
+
+    @GetMapping("api/v1/vendors/{id}")
+    Mono<Vendor> getById(@PathVariable String id){
+        return vendorRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/vendors")
+    Mono<Void> create(@RequestBody  Publisher<Vendor> vendorStream) {
+        return vendorRepository.saveAll(vendorStream).then();
+    }
+}
