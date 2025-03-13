bcf346c3ab92520c5d70d5a8e7ebf65eea6111f4
Nasim-Hasan <nasimcse2k5@gmail.com>
2025-03-13 21:46:28 +0600

    Adding 2 New Spring Boot Projects

---
+++ CurrencyConversionSampleService/src/test/java/com/currencyconversionsampleservice/CurrencyConversionSampleService/CurrencyConversionSampleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencyconversionsampleservice.CurrencyConversionSampleService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionSampleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ CurrencyExchangeSampleService/src/test/java/com/currebcyexchangesampleservice/CurrencyExchangeSampleService/CurrencyExchangeSampleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currebcyexchangesampleservice.CurrencyExchangeSampleService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeSampleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
